9e1079791720534035a7b0ca6ebca535826f93a4
moustafabazzy <bazzy.moustafa@outlook.com>
2020-12-14 23:39:58 +0200

    - MOD cart component

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription, BehaviorSubject } from 'rxjs';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent implements OnInit {
+export class CartComponent implements OnInit, OnDestroy {
+  selectedProducts$: Observable<any>;
+  selectedProducts = [];
+  subscriptions: Subscription[] = [];
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.loadSelectedProducts();
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.map(subscription => subscription.unsubscribe());
+  }
+
+  loadSelectedProducts(): void {
+    const loadSelectedProductsSubscription = this.cartService
+      .getSelectedProducts()
+      .subscribe(selectedProducts => {
+        this.selectedProducts = selectedProducts
+      });
+    this.subscriptions.push(loadSelectedProductsSubscription);
   }
 
 }
