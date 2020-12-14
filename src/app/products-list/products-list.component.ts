cd2ad310a700856a4755844c034c551fce8e4bf4
moustafabazzy <bazzy.moustafa@outlook.com>
2020-12-14 22:33:44 +0200

    - MOD products list

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -9,7 +9,31 @@ export class ProductsListComponent implements OnInit {
 
   constructor() { }
 
+  products = [
+    {
+      id: 1,
+      name: 'potatoes',
+      unit: 1,
+      price: 5
+    },
+    {
+      id: 2,
+      name: 'carrots',
+      unit: 1,
+      price: 4
+    },
+    {
+      id: 3,
+      name: 'onions',
+      unit: 1,
+      price: 2
+    },
+  ];
+
   ngOnInit(): void {
+  }
+
+  addToCart(productId: number): void {
   }
 
 }
