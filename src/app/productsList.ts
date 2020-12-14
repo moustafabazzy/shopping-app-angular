39814bc5d6eea7a562379ca2de5efef0ee530951
moustafabazzy <bazzy.moustafa@outlook.com>
2020-12-14 23:39:21 +0200

    - ADD products list data

---
+++ src/app/productsList.ts
@@ -0,0 +1,20 @@
+export const ProductsList = [
+  {
+    id: 1,
+    name: 'potatoes',
+    unit: 1,
+    price: 5
+  },
+  {
+    id: 2,
+    name: 'carrots',
+    unit: 1,
+    price: 4
+  },
+  {
+    id: 3,
+    name: 'onions',
+    unit: 1,
+    price: 2
+  },
+];
