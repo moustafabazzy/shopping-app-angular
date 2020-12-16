7fc73815d08f5448f848c8005b8aa5d5af614cac
moustafabazzy <bazzy.moustafa@outlook.com>
2020-12-16 23:21:54 +0200

    - ADD app-nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { CartComponent } from './cart/cart.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsListComponent,
-    CartComponent
+    CartComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
